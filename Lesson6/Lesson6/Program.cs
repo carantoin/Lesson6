a8062432f773634c5087b97ec42ef1ccd08a68b1
carantoin <90127329+carantoin@users.noreply.github.com>
2024-10-19 11:58:59 +0200

    Add files via upload

---
+++ Lesson6/Lesson6/Program.cs
@@ -0,0 +1,22 @@
+﻿var points = new (double x, double y)[] 
+{
+    (3.5, 7.2),
+    (-0.5, 1.2),
+    (0.72, -3.12)
+};
+for (int i = 0; i < points.Length; i++)
+{ 
+    double x = points[i].x; 
+    double y = points[i].y;
+    bool isInside = (x * x <= 2 - y) && (y >= 0); 
+        
+    if (isInside)
+    {
+        Console.WriteLine($"Точка ({x}, {y}) лежит в области, ограниченной параболой y = 2 - x^2 и осью абсцисс.");
+    }
+        
+    else
+    {
+        Console.WriteLine($"Точка ({x}, {y}) не лежит в области, ограниченной параболой y = 2 - x^2 и осью абсцисс."); 
+    }
+}
